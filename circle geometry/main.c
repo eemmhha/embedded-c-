3a540c5d99acc4a26fcef4acf6073bbc1ab1f5d7
eemmhha <eemmhha@live.com>
2017-08-26 10:17:06 +0200

    test1

---
+++ circle geometry/main.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 26, 2017
+ *      Author: eemmh
+ */
+#include "stdio.h"
+#include "math.h"
+void main ()
+{
+	char x ;
+	int r;
+	float pi=3.14;
+	printf("enter a for area and c for circ , then enter the radis value");
+	fflush(stdin); fflush(stdout);
+	scanf("%c %d",&x,&r);
+if(x=='a')
+{
+	printf("area = %f ",pi*r*r);
+}
+else if(x=='c')
+{
+	printf("circ = %f",2*pi*r);
+}
+else {
+	printf("you enter wrong char ");
+}
+
+}
